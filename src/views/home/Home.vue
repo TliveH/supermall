9f914b4d05dbf78d255c9206bbd8a754e1be6033
Tang <3366942890@qq.com>
2021-05-26 23:05:47 +0800

    初始

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,27 +3,21 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <swiper>
-      <swiper-item v-for="item in banner">
-        <a :href="item.link">
-          <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+    <home-swiper :banner="banner"></home-swiper>
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/common/navbar/NavBar"
+import NavBar from "@/components/common/navbar/NavBar";
+import HomeSwiper from './childComps/HomeSwiper';
+
 import {getHomeMultidata} from "@/network/home";
-import {Swiper, SwiperItem} from '@/components/common/swiper/index'
 
 export default {
   name: 'Home',
   components: {
     NavBar,
-    Swiper,
-    SwiperItem
+    HomeSwiper
   },
   data() {
     return {
