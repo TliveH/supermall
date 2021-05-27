2abb0ff29b1eab69f2805157ea3ad4ec0bf5bfd6
Tang <3366942890@qq.com>
2021-05-27 18:30:41 +0800

    init

--- src/views/home/childComps/FeatureView.vue
+++ src/views/home/childComps/FeatureView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="feature">
-    <a href="https://act.mogujie.com/zzlx67">
-      <img src="../../../assets/img/home/recommend_bg.jpg" alt="">
+    <a href="https://pages.tmall.com/wow/a/act/tmall/tmc/31953/4671/wupr?wh_pid=industry-242085&disableNav=YES&status_bar_transparent=true&ali_trackid=2:mm_130402922_1108650181_109788200281:1622111169_108_651657984&union_lens=lensId:OPT@1621824701@21070895_07d2_1799c4902c6_8b15@01;eventPageId:20150318020005215;recoveryid:201_11.1.240.219_5747938_1622110992889;prepvid:201_11.1.240.219_5747938_1622110992889&spm=a231o.13503973.19985496532.1&bxsign=tbk631YUlpA9r6NhwtlVQgq6fITmE6CcRmQntRNPwxk1hcACwAnuEaCoVo5G2SuzSygK+QStNpHFBFE9+lpvsKnASme0bSA44tpfwUF649/0tM=">
+      <img src="../../../assets/img/home/sunin.jpg" alt="">
     </a>
   </div>
 </template>
