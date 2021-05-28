4e24de01510a85fe1aacbdcaff04e89944a3cb32
Tang <3366942890@qq.com>
2021-05-28 08:26:35 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,22 @@
 const path = require("path");
-
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
-
 module.exports = {
   lintOnSave: true,
-  chainWebpack: (config) => {
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  // 开启 CSS source maps?
+  css:{
+    sourceMap: false,
+  },
+  chainWebpack: config => {
     config.resolve.alias
-      .set("@", resolve("./src"))
-      .set("assets", resolve("./src/assets"))
-      .set("components", resolve("./src/components"))
-      .set("network", resolve("./src/network"))
-      .set("views", resolve("./src/views"))
+      .set("@", resolve("src"))
+      .set("assets", resolve("src/assets"))
+      .set("components", resolve("src/components"))
+      .set("network", resolve("src/network"))
+      .set("views", resolve("src/views"))
   }
 }
+
